73d56d9838761243382b6d8695b1a34b2386eae5
Kafka <panxanth@gmail.com>
2019-01-15 16:40:41 +0200

    2019-01-15_01

---
+++ OTERT_Telerik/Pages/Administrator/TemplatesGlobal.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masters/Inside.Master" AutoEventWireup="true" CodeBehind="TemplatesGlobal.aspx.cs" Inherits="OTERT.Pages.Administrator.TemplatesGlobal" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="PHTitle" runat="server"><% =pageTitle %></asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="PHHead" runat="server"></asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="PHContent" runat="server">
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+        </Scripts>
+    </telerik:RadScriptManager>
+    <script type="text/javascript">
+        //Put your JavaScript code here.
+    </script>
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+        <AjaxSettings>
+            <telerik:AjaxSetting AjaxControlID="gridMain">
+                <UpdatedControls>
+                    <telerik:AjaxUpdatedControl ControlID="gridMain" LoadingPanelID="RadAjaxLoadingPanel1" />
+                    <telerik:AjaxUpdatedControl ControlID="RadWindowManager1" />
+                </UpdatedControls>
+            </telerik:AjaxSetting>
+        </AjaxSettings>
+    </telerik:RadAjaxManager>
+    <telerik:RadAjaxLoadingPanel ID="RadAjaxLoadingPanel1" runat="server" Height="75px" Width="75px" Transparency="25" InitialDelayTime="500" />
+    <div>
+        <asp:Button ID="btnPrint" runat="server" Text="Create" OnClick="btnPrint_Click" /> <asp:Button ID="btnReplace" runat="server" Text="Replace" OnClick="btnReplace_Click" />
+        <telerik:RadWindowManager RenderMode="Lightweight" ID="RadWindowManager1" runat="server" />
+    </div>
+</asp:Content>
--- OTERT_Telerik/Pages/Users/UrbanOnePoint.aspx.cs
+++ OTERT_Telerik/Pages/Users/UrbanOnePoint.aspx.cs
@@ -518,66 +518,6 @@ namespace OTERT.Pages.UserPages {
             e.File.SaveAs(System.IO.Path.Combine(fullPath, newfilename));
         }
 
-        protected void btnPrint_Click(object sender, EventArgs e) {
-            var document = new RadFlowDocument();
-            var editor = new RadFlowDocumentEditor(document);
-            editor.ParagraphFormatting.TextAlignment.LocalValue = Alignment.Justified;
-            editor.InsertLine("Dear Telerik User,");
-            editor.InsertText("We're happy to introduce the new Telerik RadWordsProcessing component for WPF. High performance library that enables you to read, write and manipulate documents in DOCX, RTF and plain text format. The document model is independent from UI and ");
-            Run run = editor.InsertText("does not require");
-            run.Underline.Pattern = UnderlinePattern.Single;
-            editor.InsertLine(" Microsoft Office.");
-            editor.InsertText("bold, ").FontWeight = System.Windows.FontWeights.Bold;
-            editor.InsertText("italic, ").FontStyle = System.Windows.FontStyles.Italic;
-            editor.InsertText("underline,").Underline.Pattern = UnderlinePattern.Single;
-            editor.InsertText(" font sizes and ").FontSize = 20;
-            editor.InsertText("colors ").ForegroundColor = new ThemableColor(System.Windows.Media.Colors.Green);
-            editor.InsertText("or ");
-            editor.InsertText("colors2 ").ForegroundColor = new ThemableColor(System.Windows.Media.Color.FromRgb(255,0,0));
-            editor.InsertLine("as well as text alignment and indentation. Other options include tables, hyperlinks, inline and floating images. Even more sweetness is added by the built-in styles and themes.");
-            editor.InsertText("Here at Telerik we strive to provide the best services possible and fulfill all needs you as a customer may have. We would appreciate any feedback you send our way through the ");
-            editor.InsertHyperlink("public forums", "http://www.telerik.com/forums", false, "Telerik Forums");
-            editor.InsertLine(" or support ticketing system.");
-            editor.InsertLine("We hope you'll enjoy RadWordsProcessing as much as we do. Happy coding!");
-            editor.InsertParagraph();
-            editor.InsertLine("Kind regards,");
-            editor.InsertLine("Telerik Admin");
-            exportDOCX(document);
-        }
-
-        protected void btnReplace_Click(object sender, EventArgs e) {
-            RadFlowDocument document2 = LoadSampleDocument();
-            RadFlowDocumentEditor editor = new RadFlowDocumentEditor(document2);
-            //System.Text.RegularExpressions.Regex textRegex = new System.Text.RegularExpressions.Regex("ΣΑΟΥΣΟΠΟΥΛΟΥ ΑΝΝΑ");
-            editor.ReplaceText("ΣΑΟΥΣΟΠΟΥΛΟΥ ΑΝΝΑ", "ΞΑΝΘΟΠΟΥΛΟΣ ΠΑΝΟΣ", true, true);
-            exportDOCX(document2);
-        }
-
-        protected void exportDOCX(RadFlowDocument doc) {
-            IFormatProvider<RadFlowDocument> formatProvider = new DocxFormatProvider();
-            byte[] renderedBytes = null;
-            using (MemoryStream ms = new MemoryStream()) {
-                formatProvider.Export(doc, ms);
-                renderedBytes = ms.ToArray();
-            }
-            Response.ClearHeaders();
-            Response.ClearContent();
-            Response.AppendHeader("content-disposition", "attachment; filename=ExportedFile.docx");
-            Response.ContentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-            Response.BinaryWrite(renderedBytes);
-            Response.End();
-        }
-
-        protected RadFlowDocument LoadSampleDocument() {
-            RadFlowDocument document2;
-            IFormatProvider<RadFlowDocument> fileFormatProvider = new DocxFormatProvider();
-            string fileName = Server.MapPath("~/Templates/testtemplate.docx");
-            using (FileStream input = new FileStream(fileName, FileMode.Open)) {
-                document2 = fileFormatProvider.Import(input);
-            }
-            return document2;
-        }
-
     }
 
 }
